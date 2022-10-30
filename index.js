266892c31df4b669ae25707eeaa8be3565958975
jenslottosen <jenslottosen@gmail.com>
2022-10-30 12:08:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = 8080;
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+const app = express();
+
+const url = "https://www.dr.dk/";
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $(".dre-article-teaser", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`server is running Hombre ${PORT}`));
